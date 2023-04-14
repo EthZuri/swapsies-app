45e8db0062165bd1c5b5a47f50778ac41b6bbd1d
David Recheni <davidrecheni@gmail.com>
2023-04-14 18:39:44 +0200

    Fix merge conflict

--- src/server/api/routers/receiver.ts
+++ src/server/api/routers/receiver.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 import Web3 from "web3";
-import { Network, Alchemy } from 'alchemy-sdk';
+import { Network, Alchemy } from "alchemy-sdk";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const receiverRouter = createTRPCRouter({
@@ -8,7 +8,11 @@ export const receiverRouter = createTRPCRouter({
     .input(z.object({ receiverAddress: z.string() }))
     .query(async ({ input }) => {
       // Connect to Alchemy
-      const web3 = new Web3(new Web3.providers.HttpProvider("https://eth-goerli.g.alchemy.com/v2/ZxqwLlN6BozfXbz4h1fS05-nu7PKnmn1"));
+      const web3 = new Web3(
+        new Web3.providers.HttpProvider(
+          "https://eth-goerli.g.alchemy.com/v2/ZxqwLlN6BozfXbz4h1fS05-nu7PKnmn1"
+        )
+      );
 
       // Get the balance of the receiver address
       const balance = await web3.eth.getBalance(input.receiverAddress);
@@ -18,13 +22,13 @@ export const receiverRouter = createTRPCRouter({
         apiKey: "ZxqwLlN6BozfXbz4h1fS05-nu7PKnmn1",
         network: Network.ETH_GOERLI,
       };
-    
+
       const alchemy = new Alchemy(settings);
       const nfts = await alchemy.nft.getNftsForOwner(input.receiverAddress);
 
       // Return the balance and NFTs
       console.log(balance, nfts);
-      
+
       return { balance, nfts };
     }),
 });
